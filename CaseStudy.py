b1387112f5081ad83055ffc09ad957408dac7dfb
MPauline26 <68587854+MPauline26@users.noreply.github.com>
2024-03-24 09:17:45 +0100

    Print plots and Corr added

--- CaseStudy.py
+++ CaseStudy.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from math import sqrt
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_excel("/Users/meikeepauline/Desktop/IP/DataPD.xlsx")
 
@@ -8,10 +10,17 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
-
-print(data)
       
 print(data.head())
 print(data.info())
 print(data.describe())
 
+""" for col in data:
+    sns.displot(data[col])
+    sns.boxplot(data[col])
+
+plt.show() """
+
+correlation = data.corr()
+
+print(correlation)
